501bfeda3ee82bd404b7e579971c4cdf42d1ffb7
Nate Droppo <nate.droppo@gce.com>
2025-07-07 17:52:02 -0700

    mvp-cleaned-up-again

--- ipyprompt/__init__.py
+++ ipyprompt/__init__.py
@@ -1,6 +1,6 @@
 from .prompt import setup_prompt
-from .magic import prompt_theme
+# from .magic import prompt_theme
 
 def load_ipython_extension(ip):
     setup_prompt(ip)
-    ip.register_magic_function(prompt_theme, 'line')
+    # ip.register_magic_function(prompt_theme, 'line')
